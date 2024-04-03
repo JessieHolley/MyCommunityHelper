22be4418cc537c86c7a38f76caab47639df56ef2
JessieHolley <jessieholley12@gmail.com>
2024-04-03 14:45:40 -0400

    classroom code

---
+++ app/src/main/java/org/pattersonclippers/mycommunityhelper/ClassRoom_Code.java
@@ -0,0 +1,14 @@
+package org.pattersonclippers.mycommunityhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassRoom_Code extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_room_code);
+    }
+}
