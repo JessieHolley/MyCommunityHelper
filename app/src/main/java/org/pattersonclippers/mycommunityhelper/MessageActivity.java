065020ab4f5cb2673c3b87fd92e9823518a9b3d9
JessieHolley <jessieholley12@gmail.com>
2024-04-15 13:24:50 -0400

    Had to take away lots of things because it was wrong.

--- app/src/main/java/org/pattersonclippers/mycommunityhelper/MessageActivity.java
+++ app/src/main/java/org/pattersonclippers/mycommunityhelper/MessageActivity.java
@@ -3,8 +3,11 @@ package org.pattersonclippers.mycommunityhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MessageActivity extends AppCompatActivity {
+
+    ImageButton PfpIB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
